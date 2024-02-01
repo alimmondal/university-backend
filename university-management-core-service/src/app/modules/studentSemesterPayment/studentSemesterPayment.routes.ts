d2c9c3679d8154e254c6972bd75a80fee04c2f48
alimmondal <italimbd@gmail.com>
2024-02-01 17:26:50 +0600

    fronted added

--- university-management-core-service/src/app/modules/studentSemesterPayment/studentSemesterPayment.routes.ts
+++ university-management-core-service/src/app/modules/studentSemesterPayment/studentSemesterPayment.routes.ts
@@ -6,9 +6,26 @@ import { StudentSemesterPaymentController } from './studentSemesterPayment.contr
 const router = express.Router();
 
 router.get(
-    '/',
-    auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.FACULTY),
-    StudentSemesterPaymentController.getAllFromDB
+  '/',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.FACULTY),
+  StudentSemesterPaymentController.getAllFromDB
+);
+
+router.get(
+  '/my-semester-payments',
+  auth(ENUM_USER_ROLE.STUDENT),
+  StudentSemesterPaymentController.getMySemesterPayments
+);
+
+router.post(
+  '/initiate-payment',
+  auth(ENUM_USER_ROLE.STUDENT),
+  StudentSemesterPaymentController.initiatePayment
+);
+router.post(
+  '/complete-payment',
+  auth(ENUM_USER_ROLE.STUDENT),
+  StudentSemesterPaymentController.completePayment
 );
 
 export const studentSemesterPaymentRoutes = router;
